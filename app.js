fe884d766096db88eac5c4d3393226b12a61e0a0
Francisco Cobas <fcarocena@gmail.com>
2020-02-14 12:10:56 -0300

    Add Players and Move routes

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
 // Connect database
 var mongoDB = process.env.DATABASE_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var db = mongoose.connection;
 db.on('error', console.log.bind(console, 'MongoDB connection error: '));
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
